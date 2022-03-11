e263acd399affc0d48ca05aedb3946f0ec0dc342
Arctur <mihailo.dilparic@gmail.com>
2022-03-11 12:57:33 +0100

    Merged branches

---
+++ timetracker/src/components/HabitCard/HabitCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function HabitCard() {
+  return (
+    <div className="habitCard">
+      <div className="habitCardContent">
+        <div className="cardContentTop">
+          <span>Work</span>
+          <span>
+            <img
+              src={`${process.env.PUBLIC_URL}/icon-ellipsis.svg`}
+              alt="DotDotDot"
+            />
+          </span>
+        </div>
+        <div className="cardContentCenter">
+          <span>32hrs</span>
+          <span>Last Week - 36hrs</span>
+        </div>
+      </div>
+    </div>
+  );
+}
