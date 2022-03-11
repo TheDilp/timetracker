3383e0fb8f4144484ce78f6f5a452215fc79a857
Arctur <mihailo.dilparic@gmail.com>
2022-03-11 12:56:29 +0100

    Created basic habit card layout

--- timetracker/src/App.js
+++ timetracker/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import HabitCard from "./components/HabitCard/HabitCard";
 import ProfileCard from "./components/ProfileCard/ProfileCard";
 
 function App() {
   return (
     <div className="App">
       <ProfileCard />
+      <HabitCard />
     </div>
   );
 }
